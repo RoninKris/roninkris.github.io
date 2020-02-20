06100f93830b14063906532b1f5ffc51c42d53a1
RoninKris <61267770+RoninKris@users.noreply.github.com>
2020-02-20 16:35:38 +0800

    Test
    Added
    * On Scroll

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let loginForm = document.loginForm;
 let registerForm = document.registerForm;
 
-function validateForm(){ //This function validates the log in forms
+function ValidateForm(){ //This function validates the log in forms
 	if(loginForm.username.value == "" || loginForm.username.value == null){
 		alert("Please enter a username");
 		return false;
@@ -9,4 +9,11 @@ function validateForm(){ //This function validates the log in forms
 	else if(loginForm.password.value < 6 && loginForm.username.value != null || loginForm.username.value != ""){
 		return true;
 	}
+}
+
+function ShowNavbar(){
+	console.log("Scrolled");
+	let navStyle = document.querySelector("nav").style;
+	navStyle.backgroundColor = "black";
+	navStyle.opacity = "0.5";
 }

de5c3ab7d84c1532549c1e308679c48866a5b72e
RoninKris <61267770+RoninKris@users.noreply.github.com>
2020-02-25 12:18:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let shown = false;
 function ShowNavbar(){
+	let shown = false;
 	if(!shown){
 		let navStyle = document.querySelector("nav").style;
 		navStyle.backgroundColor = "black";
@@ -16,7 +16,10 @@ function ShowLogin(){
 		loginDiv.style.transition = ".5s";
 		loginDiv.style.top = "15%";
 	}
-	else document.querySelector("#login-li").innerHTML = "LOG IN";
+	else{
+		document.querySelector("#login-li").innerHTML = "LOG IN";
+		loggedIn = false;
+	}
 	
 }
 
@@ -38,41 +41,47 @@ function HideLogin(){
 
 function Login(){
 	let loginForm = document.loginForm;
+	let correct;
 	const accounts = [
 	["Admin", "Ronin", "Jhane"],
 	["admin123", "Password", "Password05"]
 	];
 
+	ValidateLoginForm();
+
+	for(var i = 0; i < 3; i++){
+		if(loginForm.username.value == accounts[0][i] && loginForm.password.value == accounts[1][i]){
+			document.querySelector("#login-li").innerHTML = "LOG OUT";
+			HideLogin();
+			loggedIn = true;
+			correct = true;
+			break;
+		}
+	}
+	return false;
+}
+
+function ValidateLoginForm(correct){
 	if(loginForm.username.value == null || loginForm.username.value == ""){
 		document.querySelector("#username-span").innerHTML = "Username cannot be blank";
 		document.querySelector("#username-box").style.border = "2px solid red";
-		return false;
 	}
 	else if(loginForm.password.value == null || loginForm.password.value == ""){
 		document.querySelector("#password-span").innerHTML = "Password cannot be blank";
 		document.querySelector("#password-box").style.border = "2px solid red";
-		return false;
 	}
-	else if(loginForm.username.value == accounts[0][0] && loginForm.password.value == accounts[1][0]){
-			document.querySelector("#login-li").innerHTML = "LOG OUT";
-			HideLogin();
-			loggedIn = true;
-			return false
-	}
-	else if(loginForm.username.value == accounts[0][1] && loginForm.password.value == accounts[1][1]){
-			document.querySelector("#login-li").innerHTML = "LOG OUT";
-			HideLogin();
-			loggedIn = true;
-			return false
-	}
-	else if(loginForm.username.value == accounts[0][2] && loginForm.password.value == accounts[1][2]){
-			document.querySelector("#login-li").innerHTML = "LOG OUT";
-			HideLogin();
-			loggedIn = true;
-			return false
+	else if(loginForm.username.value != null || loginForm.username.value != "" && loginForm.password.value != null || loginForm.password.value != "" && !correct){
+		document.querySelector("#username-span").innerHTML = "Username or Password incorrect";
+		document.querySelector("#username-box").style.border = "2px solid red";
 	}
 	else{
-		alert(accounts[0][0] + " " + accounts[1][0]);
-		return false;
+		document.querySelector("#username-span").innerHTML = "";
+		document.querySelector("#username-box").style.border = "none";
+		document.querySelector("#username-box").style.borderBottom = "1px solid black";
+		document.querySelector("#password-span").innerHTML = "";
+		document.querySelector("#password-box").style.border = "none";
+		document.querySelector("#password-box").style.borderBottom = "1px solid black";
 	}
+
+	return false;
 }
